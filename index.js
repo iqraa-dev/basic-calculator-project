c4a4e0392c170b07af46deb2d8d16a233959d5be
iqraa-dev <wp23developer@gmail.com>
2026-02-05 10:22:49 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const dispaly= document.getElementById(display)
+
+function appendToDisplay(input){
+
+display.value +=input
+
+}
+
+
+function clearDisplay(){
+    display.value = ""
+
+}
+
+function calculate(){
+try{
+    display.value =eval(display.value)
+
+}
+
+catch(error){
+display.value = "Error"
+}
+
+}
